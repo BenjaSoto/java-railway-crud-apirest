09854405a13bd143048684a3576b5be69e66e512
BenjaSoto <elvissotopascacio@frba.utn.edu.ar>
2025-03-16 14:51:08 -0300

    proyecto para desplegar en railway

---
+++ src/main/java/com/benjasoto/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,11 @@
+package com.benjasoto.apirest.apirest.Repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.benjasoto.apirest.apirest.Entities.Producto;
+
+
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+
+
+}
